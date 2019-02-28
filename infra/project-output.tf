3576c1af475d6450d3b49f6d8075777cacfb0772
Justin Casali <justin.casali@colorado.edu>
2019-02-27 22:40:15 -0700

    added output

--- infra/project-output.tf
+++ infra/project-output.tf
@@ -1,1 +1,11 @@
+output "request_queue_id" {
+  value = "${aws_sqs_queue.request_queue.id}"
+}
 
+output "backup_table_id" {
+  value = "${aws_dynamodb_table.backup_table.id}"
+}
+
+output "restore_table_id" {
+  value = "${aws_dynamodb_table.restore_table.id}"
+}
