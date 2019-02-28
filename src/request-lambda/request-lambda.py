814a9adcc249b407da02d1cf97760148ad59881e
Justin Casali <justin.casali@colorado.edu>
2019-02-27 21:16:35 -0700

    prefix fix

--- src/request-lambda/request-lambda.py
+++ src/request-lambda/request-lambda.py
@@ -55,6 +55,10 @@ def lambda_handler(event, context):
         raise Exception("invalid request action")
 
 
+    # Ensure prefix ends with "/"
+    if message["bucket-prefix"][-1] != "/":
+        message["bucket-prefix"] += "/"
+
     # Write entry to db
     dynamodb.put_item(TableName=table, Item={
         "key":               {"S": message["key"]},
