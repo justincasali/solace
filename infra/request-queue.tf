e2e5f6b8b533e8109e7fff6a0f94d7e5c7ae0c85
Justin Casali <justin.casali@colorado.edu>
2019-02-28 18:07:24 -0700

    remove note

--- infra/request-queue.tf
+++ infra/request-queue.tf
@@ -2,8 +2,6 @@ resource "aws_sqs_queue" "request_queue" {
   name                       = "${var.project}-${var.release}-request-queue"
   visibility_timeout_seconds = "${var.request_timeout}"
 
-  # policy = "allow different accounts / regions access"
-
   redrive_policy = <<EOF
 {
   "deadLetterTargetArn": "${aws_sqs_queue.graveyard_queue.arn}",
