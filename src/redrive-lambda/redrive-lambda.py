4adb1b8cc0f6d3eaca3cf439e9b77d60114df4b7
Justin Casali <justin.casali@colorado.edu>
2019-03-01 17:36:52 -0700

    shut up and redrive

--- src/redrive-lambda/redrive-lambda.py
+++ src/redrive-lambda/redrive-lambda.py
@@ -1,5 +1,48 @@
+import botocore.session
+import json
+import os
+
+# AWS session
+session = botocore.session.get_session()
+
+# Dynamodb client
+dynamodb = session.create_client("dynamodb")
+
+# Backup table
+backup_table = os.environ["BACKUP_TABLE"]
+
+# Restore Table
+restore_table = os.environ["RESTORE_TABLE"]
+
+
 # Entry point
 def lambda_handler(event, context):
 
-    print("Event:", event)
-    print("Context:", context)
+    # Load message from json
+    message = json.loads(event["Records"][0]["body"])
+
+    # Backup vars
+    if message["action"] == "backup":
+
+        # Set table
+        table = backup_table
+
+    # Restore vars
+    if message["action"] == "restore":
+
+        # Set table
+        table = restore_table
+
+
+    # Increment failed segments
+    dynamodb.update_item(
+        TableName=table,
+        Key={"key": {"S": message["key"]}, "timestamp": {"N": message["timestamp"]}},
+        ExpressionAttributeNames={"#S": "failed-segments"},
+        ExpressionAttributeValues={":S": {"N": "1"}},
+        UpdateExpression="SET #S = #S + :S"
+    )
+
+
+    # Print status message
+    print(f'{message["key"]} {message["timestamp"]} segment {message["segment"]} failed')
